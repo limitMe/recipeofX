5585fdb5de6c5240eb63a9ec2f2d51e1c2a1860f
limitMe <z@limme.net>
2020-02-12 11:02:51 +0800

    Fix algorithm error

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const excelfile = "test.xlsx";
 var workbook = new Excel.Workbook();
 
 workbook.xlsx.readFile(excelfile).then(function () {
-    var worksheet = workbook.getWorksheet(1); //获取第一个worksheet
+    //获取第一个worksheet，如果数据在其他worksheet会失效
+    var worksheet = workbook.getWorksheet(1); 
 
+    //存储结果的地方，最多处理12个月的激活用户，每日用户数据不超过365天
     var results = new Array();
     for (var i = 0; i < 12; i++) {
         results[i] = new Array();
@@ -15,32 +17,40 @@ workbook.xlsx.readFile(excelfile).then(function () {
     }
 
     var startDate = convertToDate(worksheet.getCell('A2').value)
-    var endDate = convertToDate(worksheet.getCell(worksheet.actualRowCount, 1).value)
-
     worksheet.eachRow(function (row, rowNumber) {
 
-        if (rowNumber == 1) {
-            //Do nothing
+        if (rowNumber < 2) {
+            //第一行 标题省略
         } else {
             var activatedDate = convertToDate(worksheet.getCell(rowNumber, 1).value)
             var offset1 = monthDiff(startDate, activatedDate)
             row.eachCell(function (cell, colNumber) {
 
                 if (colNumber < 4) {
-                    //Do nothing
+                    //前三行，不需要在这个过程中计算
                 } else {
+                    //核心算法，依次读每个单元格，把它加到该加的位置就可以了
                     var payDate = activatedDate.addDays((colNumber - 4))
                     var offset2 = monthDiff(activatedDate, payDate)
                     results[offset1][offset2] = results[offset1][offset2] + cell.value
+                    /*
+                    var output = "现在读到第"+colNumber+"列第"+rowNumber+"行，数据是"+cell.value+
+                        "。目前用户激活于"+(activatedDate.getMonth()+1)+"月"+
+                        ",目前付费时间"+(payDate.getMonth()+1)+"月"+
+                        "。故写入数组"+offset1+","+offset2
+                    var output = "现在激活日期是"+activatedDate+",付费日期是"+payDate
+                    console.log(output)
+                    */
                 }
             });
         }
     });
 
+    //展示结果
     for (var i = 0; i < 12; i++) {
         var newline = ""
         for (var j = 0; j < 12; j++) {
-            newline = newline + " " + results[i][j]
+            newline = newline + "\t" + results[i][j]
         }
         console.log(newline)
     }
@@ -49,7 +59,8 @@ workbook.xlsx.readFile(excelfile).then(function () {
 
 function convertToDate(days) {
     var result = new Date(1900, 0, 0);
-    result.setDate(result.getDate() + days);
+    //Why we have to minors 1?
+    result.setDate(result.getDate() + days - 1);
     return result;
 }
 
@@ -62,7 +73,7 @@ Date.prototype.addDays = function(days) {
 function monthDiff(d1, d2) {
     var months;
     months = (d2.getFullYear() - d1.getFullYear()) * 12;
-    months -= d1.getMonth() + 1;
+    months -= d1.getMonth();
     months += d2.getMonth();
-    return months <= 0 ? 0 : months;
+    return months
 }
